486f6eceb46fd8e61f1123c042f578643453c09d
Joel Perren <Joel.Perren@os.uk>
2021-02-17 13:29:55 +0000

    Add express.json and cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import express = require('express');
+import cors = require('cors');
 import logger = require('./config/winstonConfig');
 import envConfig = require('./config/environmentConfig');
 
 const app: express.Application = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(envConfig.port, () => {
     logger.info(`Application started on port ${envConfig.port}`);
